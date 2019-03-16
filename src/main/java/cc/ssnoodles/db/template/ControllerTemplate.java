b96a87de98aacd7619e139da3afb085550724c8e
ssnoodles <ssnoodles0226@gmail.com>
2019-03-16 13:57:49 +0800

    added controller import

--- src/main/java/cc/ssnoodles/db/template/ControllerTemplate.java
+++ src/main/java/cc/ssnoodles/db/template/ControllerTemplate.java
@@ -4,6 +4,7 @@ import cc.ssnoodles.db.entity.Table;
 import cc.ssnoodles.db.template.classes.ClassControllerTemplateImpl;
 import cc.ssnoodles.db.template.classes.ClassNoteTemplateImpl;
 import cc.ssnoodles.db.template.functions.*;
+import cc.ssnoodles.db.template.imports.ImportControllerTemplateImpl;
 
 /**
  * @author ssnoodles
@@ -15,6 +16,8 @@ public class ControllerTemplate implements Template {
     @Override
     public String tableDataToString(Table table) {
         StringBuilder sb = new StringBuilder();
+        sb.append(new ImportControllerTemplateImpl().getTemplate());
+        sb.append(LINE);
         sb.append(new ClassNoteTemplateImpl().getTemplate(table));
         sb.append(new ClassControllerTemplateImpl().getTemplate(table));
         sb.append(new PageFunctionTemplateImpl().getTemplate(table));
---
+++ src/main/java/cc/ssnoodles/db/template/imports/ImportControllerTemplateImpl.java
@@ -0,0 +1,17 @@
+package cc.ssnoodles.db.template.imports;
+
+/**
+ * @author ssnoodles
+ * @version 1.0
+ * Create at 2019-03-16 13:05
+ */
+public class ImportControllerTemplateImpl implements ImportTemplate {
+    @Override
+    public String getTemplate() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("import org.springframework.beans.factory.annotation.Autowired;").append(LINE)
+                .append("import org.springframework.web.bind.annotation.*;").append(LINE)
+                .append("import java.util.Optional;").append(LINE);
+        return sb.toString();
+    }
+}
